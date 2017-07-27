5756d5ea6428905c074de13da4b7076e1a7c7864
Di Huynh <Di.huynh@blackbaud.com>
2017-07-27 09:52:33 -0500

    add basic test

---
+++ src/test/groovy/com/blackbaud/events/EventsSpec.groovy
@@ -0,0 +1,11 @@
+package com.blackbaud.events
+
+import spock.lang.Specification
+
+class EventsSpec extends Specification {
+
+    def "should have a basic test" () {
+        expect:
+        true
+     }
+}
